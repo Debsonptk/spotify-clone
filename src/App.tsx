cff21746addb42e61033885f9c114fcb16937298
DebsonCoelho <debsoncoelh@gmail.com>
2024-01-22 20:58:18 -0300

    add frame works

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { memo } from 'react'
 
 import Routes from 'Routes'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const App: React.FC = () => <Routes />
 
 export default memo(App)
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,15 +2,21 @@ import { memo, useEffect } from 'react'
 
 import useTitle from 'hooks/useTitle'
 
+import { ContainerBackground } from './styles'
+
 const Home: React.FC = () => {
   const setTitle = useTitle()
 
   useEffect(() => {
-    setTitle('Home | Spotify')
+    setTitle('Home ')
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return <h1>Spotify</h1>
+  return (
+    <ContainerBackground>
+      <h1 className="text-white">Spotify</h1>
+    </ContainerBackground>
+  )
 }
 
 export default memo(Home)
