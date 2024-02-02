8d5076e1ce8184bf6a45fe296d20339e44688384
DebsonCoelho <debsoncoelh@gmail.com>
2024-02-02 14:14:32 -0300

    changed layout

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,4 +2,7 @@ import styled from 'styled-components'
 
 export const ContainerBackground = styled.div`
   background-color: #000;
+  @media only screen and (max-width: 600px) {
+    /* Estilos específicos para telas menores que 600px (ajuste conforme necessário) */
+  }
 `
